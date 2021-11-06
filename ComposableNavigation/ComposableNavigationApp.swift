d851ff5a926cc627e76048835b45e935fc5e8957
Riccardo <riccardo.cipolleschi@gmail.com>
2021-11-06 14:59:45 +0100

    Initial Commit

---
+++ ComposableNavigation/ComposableNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableNavigationApp.swift
+//  ComposableNavigation
+//
+//  Created by Riccardo Cipolleschi on 06/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ComposableNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
